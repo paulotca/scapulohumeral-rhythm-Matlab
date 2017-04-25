1121696096212c3dae29aebe725d95f939ab3d08
paulotca <paulotca@gmail.com>
2017-04-25 14:38:24 -0300

    Revert "Revert "Deixando uma so tela""
    This reverts commit 93c4dbf7f9e32770507057868aef0fd08671374c.

--- telaInicial.m
+++ telaInicial.m
@@ -126,17 +126,19 @@ wh = waitbar(0,'Salvando...');steps = 100;for step = 1:30;waitbar(step / 100);en
 meshc(handles.grafico , getGlobalM);
 set(handles.grafico,'view',[0,-90]);
 
-[arquivo, caminho]=uigetfile('*.txt');
-B2=[caminho,arquivo]
-%disp(B)
-N2 = importdata(B2);
-setM2(N2)
-%r = getGlobalM
-wh = waitbar(0,'Salvando...');steps = 100;for step = 1:30;waitbar(step / 100);end 
-    for step = 30:100;waitbar(step / 100);end
-    close(wh)
-meshc(handles.axes2 , getGlobalM2);
-set(handles.axes2,'view',[0,-90]);
+%imprimir o segundo grafico
+
+% [arquivo, caminho]=uigetfile('*.txt');
+% B2=[caminho,arquivo]
+% %disp(B)
+% N2 = importdata(B2);
+% setM2(N2)
+% %r = getGlobalM
+% wh = waitbar(0,'Salvando...');steps = 100;for step = 1:30;waitbar(step / 100);end 
+%     for step = 30:100;waitbar(step / 100);end
+%     close(wh)
+% meshc(handles.axes2 , getGlobalM2);
+% set(handles.axes2,'view',[0,-90]);
 
 % hObject    handle to botaoInicial (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -159,8 +161,8 @@ function botaoFInal_Callback(hObject, eventdata, handles)
 
 hold on;
 
-Matt = getGlobalM2;
-Matt2=  getGlobalM2;
+Matt = getGlobalM;
+Matt2=  getGlobalM;
 
 ValorY=get(handles.omoY,'string');
 ValorX=get(handles.omoX,'string');
@@ -217,7 +219,7 @@ for j1 = ii:iii
 %     disp(MaiorY);
     %if MaiorX == 0 || MaiorY == 0
 %     if MaiorX ~= 0 & MaiorY ~= 0
-        Cal1(MaiorX,MaiorY)= 34;
+        Cal1(MaiorX,MaiorY)= 100;
         
         if MenorY ~= 0
         Cal2(MenorX,MenorY)= 200;
